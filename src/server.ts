ee5871f9a0ff32553f34350ab9ea3a9c97f06ce7
Duncan Sanderson <duncan@duncansanderson.co.uk>
2022-12-14 17:04:08 +0000

    FEAT (User) Add User functionality

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import morgan from 'morgan';
+// import router from './router';
+import { createNewUser, signin } from './handlers/user';
+import { protect } from './modules/auth';
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.json({ message: 'hello' });
 });
+
+// app.use('/api', router);
+
+app.use('/user', protect, createNewUser);
+app.use('/signin', signin);
 
 export default app;
