dfc73af36d8ed21a081ac9f15d599263d76bb885
Duncan Sanderson <duncan@duncansanderson.co.uk>
2023-01-27 17:43:42 +0000

    MAINT (middleware.ts) Remove console.logs

--- src/modules/middleware.ts
+++ src/modules/middleware.ts
@@ -1,9 +1,7 @@
 import { body, validationResult } from "express-validator";
 
 export const handleInputErrors = (req, res, next) => {
-    console.log('butter')
     const errors = validationResult(req);
-    console.log(errors);
 
     if (!errors.isEmpty()) {
         res.status(400);
