74f8d2f2aaa0dd358f72b5c98056aafdc1d7ed1b
Duncan Sanderson <duncan@duncansanderson.co.uk>
2025-10-16 10:46:36 +0100

    FIX db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,16 @@
 import { drizzle } from 'drizzle-orm/libsql';
 
-import * as schema from './schema';
-
 import env from '@/env';
+
+import * as schema from './schema';
 
 const db = drizzle({
     connection: {
         url: env.DATABASE_URL,
-        authToken: env.DATABASE_AUTH_TOKEN
+        authToken: env.DATABASE_AUTH_TOKEN,
     },
-    schema: { ...schema },
+    casing: 'snake_case',
+    schema,
 });
 
 export default db;
