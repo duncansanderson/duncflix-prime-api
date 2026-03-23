bb90e6a070e45c690ae88de5274235f1520b6298
Duncan Sanderson <duncan@duncansanderson.co.uk>
2026-03-23 15:40:06 +0000

    TEST (Title) Add integration tests for create route

---
+++ tests/title.test.ts
@@ -0,0 +1,72 @@
+import request from 'supertest';
+import { afterEach } from 'vitest';
+import app from '../src/server.ts';
+import { cleanupDatabase, createTestPersons, createTestUser } from './helpers/dbHelpers.ts';
+
+describe('Title endpoints', () => {
+    afterEach(() => {
+        cleanupDatabase
+    });
+
+    describe('POST /api/titles', () => {
+        const newTitle = {
+            availableSeasons: [1, 2, 5],
+            backdropPath: '/path/to/backdrop',
+            episodeRunTime: [60],
+            firstAirDate: '2024-01-04',
+            format: ['digital'],
+            genre: ['comedy', 'drama'],
+            lastAirDate: '2025-01-12',
+            name: 'TV Series Demo',
+            numberOfEpisodes: 200,
+            numberOfSeasons: 4,
+            overview: 'Summary of the series',
+            posterPath: '/path/to/poster',
+            tagline: 'Series tagline',
+            type: 'series',
+            voteAverage: 3.2,
+            voteCount: 145,
+        };
+
+        it('should successfully create a new title', async() => {
+            const { token } = await createTestUser();
+
+            const response = await request(app)
+                .post('/api/titles')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    ...newTitle,
+                });
+
+            expect(response.status).toBe(201);
+            expect(response.body).toHaveProperty('message', 'Title successfully created.');
+            expect(response.body).toHaveProperty('title');
+            expect(response.body.title.name).toBe(newTitle.name);
+        });
+
+        it('should require authentication', async() => {
+            const response = await request(app)
+                .post('/api/persons')
+                .send({
+                    ...newTitle,
+                });
+
+            expect(response.status).toBe(401);
+        });
+
+        it('should validate input data', async() => {
+            const { token } = await createTestUser();
+
+            const response = await request(app)
+                .post('/api/persons')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    ...newTitle,
+                    name: '',
+                    firstAirDate: '11 August 1956',
+                });
+
+            expect(response.status).toBe(400);
+        });
+    });
+});
